b5a50ac83d84ce518bdec28fe12bf3ebb069ad74
sunydeprito <prito.suny@gmail.comom>
2019-07-31 11:55:04 +0700

    add assets and projectsettings
    add assets and projectsettings

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class PlayerController : MonoBehaviour 
+{
+    bool isJump = true;
+    bool isDead = false;
+    int idMove = 0;
+    Animator anim;
+
+    AudioSource audio;
+    public AudioClip coinSound;
+     public AudioClip starSound;
+    
+    // Use this for initialization
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+           audio = GetComponent<AudioSource>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Jump "+isJump);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Idle();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Idle();
+        }
+        Move();
+        Dead();
+    }
+    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // Kondisi ketika menyentuh tanah
+        if (isJump)
+        {
+            anim.ResetTrigger("jump");
+            if (idMove == 0) anim.SetTrigger("idle");
+            isJump = false;
+        }
+ 
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Kondisi ketika menyentuh tanah
+        anim.SetTrigger("jump");
+        anim.ResetTrigger("run");
+        anim.ResetTrigger("idle");
+        isJump = true;
+    }
+    
+    public void MoveRight()
+    {
+        idMove = 1;
+    
+    }
+    
+    public void MoveLeft()
+    {
+        idMove = 2;
+    
+    }
+    
+    private void Move()
+    {
+        if (idMove == 1 && !isDead)
+        {
+            // Kondisi ketika bergerak ke kekanan
+            if (!isJump) anim.SetTrigger("run");
+            transform.Translate(1 * Time.deltaTime * 5f, 0, 0);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            
+        }
+        if (idMove == 2 && !isDead)
+        {
+            // Kondisi ketika bergerak ke kiri
+            if (!isJump) anim.SetTrigger("run");
+            transform.Translate(-1 * Time.deltaTime * 5f, 0, 0);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        
+        }
+    }
+    
+    public void Jump()
+    {
+        if (!isJump)
+        {
+            // Kondisi ketika Loncat           
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 300f);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.transform.tag.Equals("coin"))
+        {
+            audio.PlayOneShot(coinSound);
+            Data.score += 15;
+            Destroy(collision.gameObject);
+        }
+        if (collision.transform.tag.Equals("star"))
+        {
+            audio.PlayOneShot(starSound);
+        }
+    }
+    
+    public void Idle()
+    {
+        // kondisi ketika idle/diam
+        if (!isJump)
+        {
+            anim.ResetTrigger("jump");
+            anim.ResetTrigger("run");
+            anim.SetTrigger("idle");
+        }
+        idMove = 0;
+    }
+    
+    private void Dead()
+    {
+        if (!isDead)
+        {
+            if (transform.position.y < -10f)
+            {
+                // kondisi ketika jatuh
+                isDead = true;
+            }
+        }
+    }
+
+  
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+ 
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance = null;
+    public static SoundManager Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+ 
+    }
+}
