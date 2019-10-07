5e52c1a27c8264cd5cb9351505d5c37cdf8005a6
sunydeprito <deprito.suny@gmail.com>
2019-10-07 12:19:57 +0700

    Fix assets and project settings
    Fix all assets and project settings

---
+++ Assets/Scripts/QuitOrNext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class QuitOrNext : MonoBehaviour 
+{
+    public string NameScene = "";
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+                   SceneManager.LoadScene("Menu");
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("player"))
+        {
+            SceneManager.LoadScene(NameScene);
+        }
+    }
+
+      // pindah scene ke menu
+        public void MenuScene() {
+       SceneManager.LoadScene("Menu");
+   }
+
+  // pindah scene ke main
+        public void MainScene() {
+       SceneManager.LoadScene("Main");
+   }
+    // keluar dari game
+   public void KeluarScene() {
+       Application.Quit();
+   }
+}
---
+++ Assets/Scripts/ShowScore.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ShowScore : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Text>().text = Data.score.ToString("000");
+    }
+}
